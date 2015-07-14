eba288f90621a201872f9694aef09ab26c27e055
Ryan J. Froehlich <ryan@synergy-cns.com>
2015-07-14 11:14:08 -0500

    Format & finish server handler

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
+// Load dependencies
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Start Express
 var app = express();
 
-// view engine setup
+// Setup view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Add app dependencies
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
+// GET route
+app.get('/', function(req, res)
+{
 
-    if(req.query.CallID) {
+    // Check for CallID
+    if(req.query.CallID)
+    {
 
         app.emit('incomingCallEvent', req.query);
         res.sendStatus(200);
@@ -28,14 +32,19 @@ app.get('/', function(req, res) {
     }
     else
     {
+
         res.sendStatus(500);
-    }
+
+    } // End if
 
 });
 
-app.post('/', function(req, res) {
+// POST route
+app.post('/', function(req, res)
+{
 
-    if(req.body.CallID) {
+    if(req.body.CallID)
+    {
 
         app.emit('incomingCallEvent', req.body);
         res.sendStatus(200);
@@ -43,41 +52,51 @@ app.post('/', function(req, res) {
     }
     else
     {
+
         res.sendStatus(500);
-    }
+
+    } // End if
 
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next)
+{
+
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
+
 });
 
-// error handlers
+// Development error handler - will print stacktrace
+if (app.get('env') === 'development')
+{
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res) {
+    app.use(function(err, req, res)
+    {
+
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
             error: err
         });
-    });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res) {
+    });
+
+} // End if
+
+// Production error handler - no stacktraces leaked to user
+app.use(function(err, req, res)
+{
+
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
+
 });
 
-
+// Export
 module.exports = app;
--- server.js
+++ server.js
@@ -1,180 +1,291 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
+// Load dependencies
 var app = require('./app');
 var debug = require('debug')('eAgent Server:server');
 var http = require('http');
 var net = require('net');
 
-/**
- * Get port from environment and store in Express.
- */
-
+// Get port from environment and store in Express.
 var port = normalizePort(process.env.PORT || '80');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
-
+// Create HTTP server
 var server = http.createServer(app);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-
+// Listen on provided port, on all network interfaces.
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
+// Listing for incoming events
+app.on('incomingCallEvent', onIncomingCall);
+
+/**
+ * Setup socket server
+ */
+// Keep track of the socket clients
+var arrClients = [];
+
+// Start a TCP Socket Server
+net.createServer(function (socket)
+{
+
+    // Handle incoming messages from clients.
+    socket.on('data', function (data)
+    {
+
+        // Check for data
+        if(data)
+        {
+
+            // Split into parts
+            console.log('Incoming socket data: ' + data);
+            var arrDataParts = data.toString().split(' ');
+            var strCommand = arrDataParts[0];
+
+            // Client is identifying themselves
+            if (strCommand == 'IDENTIFY_EXTENSION')
+            {
+
+                // Setup local vars
+                var strClientKey = arrDataParts[1].toString();
+                var strExtensionKey = arrDataParts[2].toString();
+
+                // Assign to socket
+                socket.sClientID = strClientKey;
+                socket.sExtensionNum = strExtensionKey;
+
+                console.log('Adding socket with Client ID: ' + arrDataParts[1] + ' and Extension: ' + arrDataParts[2]);
+
+                // Check for client key in clients array
+                if(!arrClients[strClientKey])
+                {
+
+                    arrClients[strClientKey] = [];
+
+                } // End if
+
+                // Add socket to array
+                arrClients[strClientKey].push(socket);
+
+            }
+            else if(strCommand == 'PING')
+            {
+
+                // Respond to heartbeat
+                socket.write('PONG');
+
+            } // End if
+
+        } // End if
+
+    });
+
+    // Remove the client from the list when it leaves
+    socket.on('close', function ()
+    {
+
+        console.log('Socket closing %o', arrClients);
+        removeSocket(socket);
+
+    });
+
+    // Remove the client from the list when it leaves
+    socket.on('end', function ()
+    {
+
+        console.log('Socket ending');
+        removeSocket(socket);
+
+    });
+
+    // Error handler
+    socket.on('error', function (error)
+    {
+
+        console.log('Socket error %o', error);
+
+    });
+
+}).listen(81).on('listening', onSocketServerListening);
+
+/**
+ * Broadcast message to specific extension
+ *
+ * @param   strClientID             Extension's ClientID
+ * @param   strExtensionNumber      Extension number
+ * @param   strMessage              Message to send
+ */
+function broadcastToExtension(strClientID, strExtensionNumber, strMessage)
+{
+    console.log('Broadcasting to Extension: ' + strExtensionNumber + ' under Client ID: ' + strClientID);
+
+    // Check if ClientID exists
+    if(Array.isArray(arrClients[strClientID]))
+    {
+
+        // Iterate client's socket connections
+        arrClients[strClientID].every(function(socSocket)
+        {
+
+            // Check if extension numbers match
+            if(socSocket.sExtensionNum == strExtensionNumber)
+            {
+
+                // Write message & exit loop
+                socSocket.write(strMessage);
+                return false;
+
+            }
+            else
+            {
+
+                // Continue loop
+                return true;
+
+            } // End if
+
+        });
+
+    } // End if
+
+} // End function
+
 /**
  * Normalize a port into a number, string, or false.
+ *
+ * @param   val
+ * @returns {*}
  */
+function normalizePort(val)
+{
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+    var port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+    if (isNaN(port))
+    {
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+        // named pipe
+        return val;
 
-  return false;
-}
+    } // End if
+
+    if (port >= 0)
+    {
+
+        // port number
+        return port;
+
+    } // End if
+
+    return false;
+
+} // End function normalizePort
 
 /**
  * Event listener for HTTP server "error" event.
+ *
+ * @param error
  */
+function onError(error)
+{
 
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+    if (error.syscall !== 'listen')
+    {
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+        throw error;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
+    } // End if
 
-app.on('incomingCallEvent', onIncomingCall);
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
 
+    // handle specific listen errors with friendly messages
+    switch (error.code)
+    {
+
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+
+        default:
+            throw error;
+
+    } // End switch
+
+} // End function onError
+
+/**
+ * Handler for when an incoming call is received
+ *
+ * @param   objCallData
+ * @return  void
+ */
 function onIncomingCall(objCallData)
 {
 
     var arrExtensionParts = objCallData.CalledExtension.split('*');
     broadcastToExtension(arrExtensionParts[0], arrExtensionParts[1], JSON.stringify(objCallData));
 
-}
+} // End function onIncomingCall
 
 /**
  * Event listener for HTTP server "listening" event.
+ *
+ * @return  void
  */
+function onListening()
+{
 
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+
+    debug('Listening on ' + bind);
+
+} // End function onListening
 
 /**
- * Setup socket server
+ * Handler for when the socket server starts
+ *
+ * @return  void
  */
-
-// Keep track of the chat clients
-var clients = [];
-
-// Start a TCP Server
-net.createServer(function (socket) {
-
-  // Handle incoming messages from clients.
-  socket.on('data', function (data) {
-
-    // Check for data
-    if(data) {
-
-      // Split into parts
-      var arrDataParts = data.split(' ');
-      var strCommand = arrDataParts[0];
-
-      // Client is identifying themselves
-      if (strCommand == 'IDENTIFY_CLIENT') {
-        if (!clients[arrDataParts[1]])
-        {
-          clients[arrDataParts[1]] = [];
-        }
-        console.log('Adding socket with Client ID: ' + arrDataParts[1] + ' and Extension: ' + arrDataParts[2]);
-        clients[arrDataParts[1]][arrDataParts[2]] = socket;
-      }
-
-    }
-
-  });
-
-  // Remove the client from the list when it leaves
-  socket.on('end', function () {
-    clients.every(function(arrClient) {
-      if(arrClient.indexOf(socket) > -1)
-      {
-        arrClient.splice(arrClient.indexOf(socket), 1);
-        return false;
-      } else {
-        return true;
-      }
-    });
-  });
-
-}).listen(81);
-
-// Send message to all extensions belonging to a specific client
-/*
-function broadcastToClient(strClientID, strMessage)
+function onSocketServerListening()
 {
-    console.log('Broadcsting to Client ID: ' + strClientID);
-    if(Array.isArray(clients[strClientID]))
-    {
-        clients[strClientID].forEach(function (socSocket) {
-            socSocket.write(strMessage);
-        });
-    }
-}
-*/
 
-// Send message to specific extension
-function broadcastToExtension(strClientID, strExtensionNumber, strMessage)
+    console.log('Socket server started on port %s at %s', socketServer.address().port, socketServer.address().address);
+
+} // End function onSocketServerListening
+
+/**
+ * Remove socket from array
+ *
+ * @param socket
+ * @return  void
+ */
+function removeSocket(socket)
 {
-    console.log('Broadcasting to Extension: ' + strExtensionNumber + ' under Client ID: ' + strClientID);
-    if(Array.isArray(clients[strClientID]))
-    {
-        if(strExtensionNumber in clients[strClientID])
-        {
-            clients[strClientID][strExtensionNumber].write(strMessage);
-        }
-    }
-}
 
-// Put a friendly message on the terminal of the server.
-console.log("Socker server running at port 81\n");
+    // Look for ClientID in array
+    if(arrClients[socket.sClientID].indexOf(socket) > -1)
+    {
+
+        console.log('Removing socket from array at position: ' + arrClients[socket.sClientID].indexOf(socket));
+
+        // Remove socket
+        arrClients[socket.sClientID].splice(arrClients[socket.sClientID].indexOf(socket), 1);
+
+    } // End if
+
+} // End function removeSocket
